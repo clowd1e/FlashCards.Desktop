c348cce55cce5eced3d5fa695a5add1e54015eea
savchenkoadd <savchenkooad@gmail.com>
2024-01-28 20:48:46 +0200

    Add identity models

---
+++ Models/Identity/RegisterDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace FlashCards.Desktop.Models.Identity
+{
+	public class RegisterDTO
+	{
+		public string? PersonName { get; set; }
+
+		public string? Email { get; set; }
+
+		public string? PhoneNumber { get; set; }
+
+		public string? Password { get; set; }
+
+		public string? ConfirmPassword { get; set; }
+	}
+}
