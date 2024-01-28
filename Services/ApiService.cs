92506b375f904722de3c970417c0c2d00f1dae25
savchenkoadd <savchenkooad@gmail.com>
2024-01-28 21:20:13 +0200

    Implement ApiService

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -1,5 +1,9 @@
-﻿using FlashCards.Desktop.Models.Identity;
+﻿using FlashCards.Desktop.Helpers;
+using FlashCards.Desktop.Models;
+using FlashCards.Desktop.Models.Identity;
+using Newtonsoft.Json;
 using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace FlashCards.Desktop.Services
 {
@@ -21,7 +25,37 @@ namespace FlashCards.Desktop.Services
 
         public async Task Register(RegisterDTO? registerDTO)
         {
+            await ValidationHelper.ValidateObjects(registerDTO);
 
+            await _httpClient.PostAsJsonAsync(_connectionString + nameof(Register), registerDTO);
         } 
+
+        public async Task Login(LoginDTO? loginDTO)
+        {
+            await ValidationHelper.ValidateObjects(loginDTO);
+
+			await _httpClient.PostAsJsonAsync(_connectionString + nameof(Login), loginDTO);
+		}
+
+        public async Task Logout()
+        {
+            await _httpClient.GetAsync(_connectionString + nameof(Logout));
+        }
+
+        public async Task<AffectedResponse> SyncCards(List<Flashcard>? flashcards)
+        {
+            await ValidationHelper.ValidateObjects(flashcards);
+
+            var responseMessage = await _httpClient.PostAsJsonAsync(_connectionString + nameof(SyncCards), flashcards);
+
+            var affectedResponse = await responseMessage.Content.ReadFromJsonAsync<AffectedResponse>();
+
+            if (affectedResponse is null)
+            {
+                throw new JsonSerializationException("Failed to deserialize response.");
+            }
+
+            return affectedResponse;
+        }
     }
 }
