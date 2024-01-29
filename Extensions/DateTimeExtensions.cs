38e850508aee9dcb738159ddb8ab801a2235cc10
savchenkoadd <savchenkooad@gmail.com>
2024-01-29 19:17:41 +0200

    Add DateTime extensions

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace FlashCards.Desktop.Extensions
+{
+	public static class DateTimeExtensions
+	{
+		public static DateOnly ToDateOnly(this DateTime date)
+		{
+			return DateOnly.FromDateTime(date);
+		}
+	}
+}
