3680c093b82c886aa9e218c5c5084caf3481e3d3
savchenkoadd <savchenkooad@gmail.com>
2024-01-29 20:48:03 +0200

    Change api repository

--- Repositories/ApiRepository.cs
+++ Repositories/ApiRepository.cs
@@ -7,63 +7,63 @@ using System.Net.Http.Json;
 
 namespace FlashCards.Desktop.Repositories
 {
-    public class ApiRepository
-    {
-        private readonly HttpClient _httpClient;
-        private readonly string _connectionString;
+	public class ApiRepository
+	{
+		private readonly HttpClient _httpClient;
+		private readonly string _connectionString;
 
-        public ApiRepository(string? connectionString)
-        {
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                throw new ArgumentException("Connection string cannot be null or empty");
-            }
+		public ApiRepository(string? connectionString)
+		{
+			if (string.IsNullOrEmpty(connectionString))
+			{
+				throw new ArgumentException("Connection string cannot be null or empty");
+			}
 
-            _httpClient = new HttpClient();
-            _connectionString = connectionString;
-        }
+			_httpClient = new HttpClient();
+			_connectionString = connectionString;
+		}
 
-        public async Task Register(RegisterDTO? registerDTO)
-        {
-            await ValidationHelper.ValidateObjects(registerDTO);
+		public async Task<HttpResponseMessage> Register(RegisterDTO registerDTO)
+		{
+			var result = await _httpClient.PostAsJsonAsync(_connectionString + nameof(Register), registerDTO);
 
-            var result = await _httpClient.PostAsJsonAsync(_connectionString + nameof(Register), registerDTO);
+			return result;
+		}
 
-            result.EnsureSuccessStatusCode();
-        }
+		public async Task<HttpResponseMessage> Login(LoginDTO loginDTO)
+		{
+			var result = await _httpClient.PostAsJsonAsync(_connectionString + nameof(Login), loginDTO);
 
-        public async Task Login(LoginDTO? loginDTO)
-        {
-            await ValidationHelper.ValidateObjects(loginDTO);
+			return result;
+		}
 
-            var result = await _httpClient.PostAsJsonAsync(_connectionString + nameof(Login), loginDTO);
+		public async Task<HttpResponseMessage> Logout()
+		{
+			var result = await _httpClient.GetAsync(_connectionString + nameof(Logout));
 
-            result.EnsureSuccessStatusCode();
-        }
+			return result;
+		}
 
-        public async Task Logout()
-        {
-            var result = await _httpClient.GetAsync(_connectionString + nameof(Logout));
+		public async Task<List<Flashcard>?> GetAllCards()
+		{
+			var result = await _httpClient.GetAsync(_connectionString + nameof(GetAllCards));
 
-            result.EnsureSuccessStatusCode();
-        }
+			result.EnsureSuccessStatusCode();
 
-        public async Task<AffectedResponse> SyncCards(List<Flashcard>? flashcards)
-        {
-            await ValidationHelper.ValidateObjects(flashcards);
+			var cards = await result.Content.ReadFromJsonAsync<List<Flashcard>>();
 
-            var responseMessage = await _httpClient.PostAsJsonAsync(_connectionString + nameof(SyncCards), flashcards);
+			return cards;
+		}
 
-            responseMessage.EnsureSuccessStatusCode();
+		public async Task<AffectedResponse?> SyncCards(List<Flashcard> flashcards)
+		{
+			var responseMessage = await _httpClient.PostAsJsonAsync(_connectionString + nameof(SyncCards), flashcards);
 
-            var affectedResponse = await responseMessage.Content.ReadFromJsonAsync<AffectedResponse>();
+			responseMessage.EnsureSuccessStatusCode();
 
-            if (affectedResponse is null)
-            {
-                throw new JsonSerializationException("Failed to deserialize response.");
-            }
+			var affectedResponse = await responseMessage.Content.ReadFromJsonAsync<AffectedResponse>();
 
-            return affectedResponse;
-        }
-    }
+			return affectedResponse;
+		}
+	}
 }
---
+++ Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using FlashCards.Desktop.Helpers;
+using FlashCards.Desktop.Models.Identity;
+using FlashCards.Desktop.Repositories;
+using FlashCards.Desktop.ServiceContracts;
+
+namespace FlashCards.Desktop.Services
+{
+	public class UserService : IUserService
+	{
+        private readonly ApiRepository _apiRepository;
+		private bool _loggedIn = false;
+
+        public UserService(ApiRepository apiRepository)
+        {
+            _apiRepository = apiRepository;
+        }
+
+		public async Task Login(LoginDTO? loginDTO)
+		{
+			await ValidationHelper.ValidateObjects(loginDTO);
+
+			if (_loggedIn)
+			{
+				throw new InvalidOperationException("Already logged in.");
+			}
+
+			await _apiRepository.Login(loginDTO);
+		}
+
+		public async Task Logout()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task Register(RegisterDTO? registerDTO)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
