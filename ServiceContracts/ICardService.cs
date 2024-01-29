5fa5f8880ef22eb7d01fb77d37aa9a4779fb716c
savchenkoadd <savchenkooad@gmail.com>
2024-01-29 19:41:00 +0200

    Add card service interface

---
+++ ServiceContracts/ICardService.cs
@@ -0,0 +1,21 @@
+﻿using FlashCards.Desktop.Models;
+
+namespace FlashCards.Desktop.ServiceContracts
+{
+	public interface ICardService
+	{
+		Task ReviewCard(Guid cardId, int responseQuality);
+
+		Task UpdateCard(Guid id, string mainSide, string oppositeSide);
+
+		Task CreateCard(Flashcard flashcard);
+
+		Task DeleteCard(Guid id);
+
+		Task<IEnumerable<Flashcard>?> GetCardsToReview();
+
+		Task<IEnumerable<Flashcard>?> GetAllCards();
+
+		Task<Flashcard> GetCardById(Guid id);
+	}
+}
