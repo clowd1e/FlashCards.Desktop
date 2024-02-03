73e870f29625388c28eca8f32fd81810c44f7aed
Лобченко Олександр <sashapro712@gmail.com>
2024-02-03 18:49:07 +0100

    Adding Show answer command

---
+++ Commands/ShowAnswerCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards.Desktop.Commands
+{
+    public class ShowAnswerCommand : CommandBase
+    {
+        public ShowAnswerCommand() { }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
