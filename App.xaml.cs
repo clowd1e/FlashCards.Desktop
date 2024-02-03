628595d8b909ce018a8be885b77a3277063ebe8a
Лобченко Олександр <sashapro712@gmail.com>
2024-02-03 18:50:04 +0100

    Updating Views

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using FlashCards.Desktop.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,16 @@ namespace FlashCards.Desktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
