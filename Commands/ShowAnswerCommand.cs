b4669613eb8cf2c5263f9bb77bf64b0bd5a99c0a
Лобченко Олександр <sashapro712@gmail.com>
2024-02-03 19:15:27 +0100

    Implementing Show answer command

--- Commands/ShowAnswerCommand.cs
+++ Commands/ShowAnswerCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FlashCards.Desktop.Models;
+using FlashCards.Desktop.Stores;
+using FlashCards.Desktop.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +11,20 @@ namespace FlashCards.Desktop.Commands
 {
     public class ShowAnswerCommand : CommandBase
     {
-        public ShowAnswerCommand() { }
+        private NavigationStore _navigationStore;
+        private MainViewModel _mainViewModel;
+        private Flashcard _flashcard;
+        public ShowAnswerCommand(NavigationStore navigationStore, MainViewModel mainViewModel, Flashcard flashcard)
+        {
+            _navigationStore = navigationStore;
+            _mainViewModel = mainViewModel;
+            _flashcard = flashcard;
+        }
 
         public override void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            _navigationStore.CurrentFlashCard = new FlashCardBackViewModel(_flashcard);
+            _mainViewModel.IsFeedbackPanelVisibile = true;
         }
     }
 }
