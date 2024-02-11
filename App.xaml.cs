e1b20fe74c5bd26750d17902be56a98ea365aa29
Олександр Лобченко <sashapro712@gmail.com>
2024-02-11 21:14:20 +0100

    Creating Login Window

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,11 +12,17 @@ namespace FlashCards.Desktop
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow = new MainWindow()
+            MainWindow = new LoginWindow()
             {
-                DataContext = new MainViewModel()
+                DataContext = new LoginViewModel()
             };
             MainWindow.Show();
+
+            //MainWindow = new MainWindow()
+            //{
+            //    DataContext = new MainViewModel()
+            //};
+            //MainWindow.Show();
 
             base.OnStartup(e);
         }
