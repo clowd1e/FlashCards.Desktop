b8df162ba26ca3f4e4d394a9df2fa16aaaa357df
Олександр Лобченко <sashapro712@gmail.com>
2024-02-21 18:17:15 +0100

    Adding command for submitting registration

---
+++ ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FlashCards.Desktop.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlashCards.Desktop.ViewModels
+{
+    public class RegistrationViewModel : ViewModelBase
+    {
+        public ICommand Submit { get; }
+        public RegistrationViewModel()
+        {
+            Submit = new RegAccountCommand(this);
+        }
+    }
+}
