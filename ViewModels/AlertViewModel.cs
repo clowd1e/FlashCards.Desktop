b2eb608f5433ea0169f0e8ebd2ac54f606ec1eab
Олександр Лобченко <sashapro712@gmail.com>
2024-02-21 18:11:21 +0100

    Adding and implementing AlertViewModel for AlertWindow

---
+++ ViewModels/AlertViewModel.cs
@@ -0,0 +1,45 @@
+﻿using FlashCards.Desktop.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FlashCards.Desktop.ViewModels
+{
+    public class AlertViewModel : ViewModelBase
+    {
+        private string headerText;
+        public string HeaderText
+        {
+            get => headerText;
+            set
+            {
+                headerText = value;
+                OnPropertyChanged(nameof(HeaderText));
+            }
+        }
+
+        private string infoText;
+        public string InfoText
+        {
+            get => infoText;
+            set
+            {
+                infoText = value;
+                OnPropertyChanged(nameof(InfoText));
+            }
+        }
+
+        public ICommand Continue { get; }
+
+        public AlertViewModel(Window window, string headerText, string infoText)
+        {
+            HeaderText = headerText;
+            InfoText = infoText;
+            Continue = new ExitAlertCommand(window, new LoginWindow(), new LoginViewModel());
+        }
+    }
+}
