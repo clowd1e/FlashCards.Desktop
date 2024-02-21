8a36a3b4a02a6cd8f19bb3ecd4ae9e28026138b1
Олександр Лобченко <sashapro712@gmail.com>
2024-02-21 18:16:44 +0100

    Adding Command for Sing up button

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using FlashCards.Desktop.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FlashCards.Desktop.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
+        
+        public ICommand SingUp { get; }
+        public LoginViewModel()
+        {
+            SingUp = new ChangeToRegFormCommand(this);
+        }
     }
 }
